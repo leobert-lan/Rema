02c6e4092a7ea8f8df94190c3a216e57877c6701
leobert-lan <774057695@qq.com>
2021-05-19 17:59:13 +0800

    feat:add doc for Job.kt

---
+++ rema/src/main/java/osp/leobert/android/rema/cache/CacheActionHandler.kt
@@ -0,0 +1,9 @@
+package osp.leobert.android.rema.cache
+
+/**
+ * <p><b>Package:</b> osp.leobert.android.rema.cache </p>
+ * <p><b>Classname:</b> CacheActionHandler </p>
+ * Created by leobert on 2021/5/19.
+ */
+class CacheActionHandler {
+}
--- rema/src/main/java/osp/leobert/android/rema/core/Job.kt
+++ rema/src/main/java/osp/leobert/android/rema/core/Job.kt
@@ -4,6 +4,9 @@ package osp.leobert.android.rema.core
  * <p><b>Package:</b> osp.leobert.android.rema.core </p>
  * <p><b>Classname:</b> Job </p>
  * Created by leobert on 2021/5/19.
+ *
+ * contains Model-Loading,Cache-Maintain, it's re-usable when it's state is not finished,thus,
+ * the same [Request] may share one [Job]
  */
 class Job {
 }
